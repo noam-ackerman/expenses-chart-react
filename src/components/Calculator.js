f7e50553208b7e388702f9877b078b1f1ccd01d7
Noam Ackerman <developer.noam@gmail.com>
2023-03-30 22:10:41 +0300

    fixing event handle

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,17 +4,14 @@ import "../style/Calculator.css";
 export default function Calculator(props) {
     let calculatorModal = React.useRef();
     let calculatorOverLay = React.useRef();
-    
-    React.useEffect(() => {
-        calculatorOverLay.current.addEventListener("click", function(event){
-            if(event.target !== calculatorModal.current) {
-              props.toggleCalculator(props.calculatorMode);
-            }
-        })
-    }, [props])
- 
 
-    return <div  ref={calculatorOverLay} class="calculatorOverlay">
+    function handleOverlayClick(event){
+        if(event.target !== calculatorModal.current) {
+            props.toggleCalculator(props.calculatorMode);
+        }
+    }
+    
+    return <div  ref={calculatorOverLay} class="calculatorOverlay" onClick={handleOverlayClick}>
         <div ref={calculatorModal} class="calculator-modal bg-gradient shadow-sm"></div>
     </div>
 }
