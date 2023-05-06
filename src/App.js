45876c03516e9e1b98d0a2b0e99d7175ee5a8f01
Noam Ackerman <developer.noam@gmail.com>
2023-05-06 16:12:59 +0300

    adding a footer

--- src/App.js
+++ src/App.js
@@ -71,29 +71,50 @@ function App() {
 
 
    return (
-    <div className="bg-gradient p-3">
-      <img src="https://images.cooltext.com/5650973.png" alt="expenses-chart-title" className="title-image" style={{ opacity : imageLoaded ? 1 : 0}} onLoad={() => setImageLoaded(true)}/>
-      {imageLoaded ?
-        <>
-          <NewExpense onAddExpense={handleAddExpense} selectedCurrency={selectedCurrency} sendCurrency={sendCurrency}/>
-          {expenses.length > 0 ? <ExpensesFilter years={years} sendSelectedYear={SelectedYear} selectedYear={selectedYear}/> : null }
-          {expenses.length > 0 ? <ExpensesChart expenses={expenses} years={years} selectedYear={selectedYear}/> : null}
-          <Expenses data={expenses} currency={selectedCurrency} selectedYear={selectedYear} sendDeletedItem={sendDeletedItem} itemEdit={itemEdit}/>
-        </>
-      :
-       <div className="Loader">
-        <Hearts 
-          height="180"
-          width="180"
-          color="#fdb4ff"
-          ariaLabel="hearts-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
-        />
-       </div>
-      }   
-    </div>
+    <>
+      <div className="bg-gradient p-3 pb-5">
+        <img src="https://images.cooltext.com/5650973.png" alt="expenses-chart-title" className="title-image" style={{ opacity : imageLoaded ? 1 : 0}} onLoad={() => setImageLoaded(true)}/>
+        {imageLoaded ?
+          <>
+            <NewExpense onAddExpense={handleAddExpense} selectedCurrency={selectedCurrency} sendCurrency={sendCurrency}/>
+            {expenses.length > 0 ? <ExpensesFilter years={years} sendSelectedYear={SelectedYear} selectedYear={selectedYear}/> : null }
+            {expenses.length > 0 ? <ExpensesChart expenses={expenses} years={years} selectedYear={selectedYear}/> : null}
+            <Expenses data={expenses} currency={selectedCurrency} selectedYear={selectedYear} sendDeletedItem={sendDeletedItem} itemEdit={itemEdit}/>
+          </>
+        :
+        <div className="Loader">
+          <Hearts 
+            height="180"
+            width="180"
+            color="#fdb4ff"
+            ariaLabel="hearts-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+        }   
+      </div>
+      <footer>
+        <a
+          href="https://github.com/noam-ackerman/expenses-chart-react"
+          target="_blank"
+          rel="noreferrer"
+          className="codeLink"
+        >
+          Open-source code
+        </a>{" "}
+        designed and built by{" "}
+        <a
+          href="https://www.linkedin.com/in/noam-ackerman/"
+          target="_blank"
+          rel="noreferrer"
+          className="codeLink"
+        >
+          Noam Ackerman
+        </a>
+      </footer>
+    </>
   );
 }
 
